5a4909ffff13fa53600857cf9959c7f3842c1e47
caio-fernadiz <arostuche@gmail.com>
2025-04-28 09:14:32 -0300

    git criando função de updatebook no bookservice

--- src/main/java/com/biblioteca/biblioteca_avaliar/book/service/BookService.java
+++ src/main/java/com/biblioteca/biblioteca_avaliar/book/service/BookService.java
@@ -2,8 +2,11 @@ package com.biblioteca.biblioteca_avaliar.book.service;
 
 import com.biblioteca.biblioteca_avaliar.book.bookDTO.BookDTO;
 import com.biblioteca.biblioteca_avaliar.book.bookDTO.BookFormDTO;
+import com.biblioteca.biblioteca_avaliar.book.bookDTO.BookFormUpdateDTO;
 
 public interface BookService {
 
     BookDTO saveBook(BookFormDTO form);
+
+    BookDTO updateBook(Long id, BookFormUpdateDTO form);
 }
