d79adc7d492ec7db750784eed1a04ede33fa7906
caio-fernadiz <arostuche@gmail.com>
2025-04-28 09:06:07 -0300

    criando repository do book

---
+++ src/main/java/com/biblioteca/biblioteca_avaliar/book/BookRepository.java
@@ -0,0 +1,7 @@
+package com.biblioteca.biblioteca_avaliar.book;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+}
