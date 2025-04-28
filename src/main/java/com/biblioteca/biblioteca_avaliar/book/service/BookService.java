a90fad4636a942711c73659277ff2a580fde4175
caio-fernadiz <arostuche@gmail.com>
2025-04-28 09:13:55 -0300

    git criando função de savebook no bookservice

--- src/main/java/com/biblioteca/biblioteca_avaliar/book/service/BookService.java
+++ src/main/java/com/biblioteca/biblioteca_avaliar/book/service/BookService.java
@@ -1,5 +1,9 @@
 package com.biblioteca.biblioteca_avaliar.book.service;
 
+import com.biblioteca.biblioteca_avaliar.book.bookDTO.BookDTO;
+import com.biblioteca.biblioteca_avaliar.book.bookDTO.BookFormDTO;
+
 public interface BookService {
 
+    BookDTO saveBook(BookFormDTO form);
 }
