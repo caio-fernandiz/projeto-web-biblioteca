e42440d2bc388700f68ee44748798b068fd5529f
caio-fernadiz <arostuche@gmail.com>
2025-04-28 08:50:30 -0300

    criando model do book

---
+++ src/main/java/com/biblioteca/biblioteca_avaliar/book/Book.java
@@ -0,0 +1,25 @@
+package com.biblioteca.biblioteca_avaliar.book;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Data
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String author;
+    private String synopsis;
+    private String releaseDate;
+    private String publisher;
+}
