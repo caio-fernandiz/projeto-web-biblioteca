bfac3692fe54b3925c73c5bc719eb9a92d5925f6
caio-fernadiz <arostuche@gmail.com>
2025-04-28 08:39:58 -0300

    criando o projeto e atualizando o applicationproprieties

---
+++ src/main/java/com/biblioteca/biblioteca_avaliar/BibliotecaAvaliarApplication.java
@@ -0,0 +1,13 @@
+package com.biblioteca.biblioteca_avaliar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaAvaliarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaAvaliarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/biblioteca/biblioteca_avaliar/BibliotecaAvaliarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biblioteca.biblioteca_avaliar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaAvaliarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
