20fd4a62d05645007bc5fbbdd1b2a23c017f1561
caio-fernadiz <arostuche@gmail.com>
2025-04-29 11:13:48 -0300

    criando endpoint post no bookcontroller

--- src/main/java/com/biblioteca/biblioteca_avaliar/book/BookController.java
+++ src/main/java/com/biblioteca/biblioteca_avaliar/book/BookController.java
@@ -1,5 +1,35 @@
 package com.biblioteca.biblioteca_avaliar.book;
 
-public class BookController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.biblioteca.biblioteca_avaliar.book.bookDTO.BookDTO;
+import com.biblioteca.biblioteca_avaliar.book.bookDTO.BookFormDTO;
+import com.biblioteca.biblioteca_avaliar.book.service.BookService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/biblioteca")
+public class BookController {
+    
+    private final BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @PostMapping
+    public ResponseEntity<BookDTO> saveBook(@Valid @RequestBody BookFormDTO bookFormDTO) {
+        BookDTO bookDTO = bookService.saveBook(bookFormDTO);
+        return new ResponseEntity<>(bookDTO, HttpStatus.CREATED);
+    }
 }
