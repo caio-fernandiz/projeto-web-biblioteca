205b1286c935b85b4b51d0d8006bddbf0167635a
caio-fernadiz <arostuche@gmail.com>
2025-04-29 11:15:57 -0300

    adicionando existsbyisbn no bookrepository

--- src/main/java/com/biblioteca/biblioteca_avaliar/book/BookRepository.java
+++ src/main/java/com/biblioteca/biblioteca_avaliar/book/BookRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
 
+    boolean existsByIsbn(String isbn);
     Optional<Book> findByIsbn(String isbn);
     Optional<Book> findByTitle(String title);
     Optional<Book> findByAuthor(String author);
