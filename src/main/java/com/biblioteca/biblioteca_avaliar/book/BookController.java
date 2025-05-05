52dda3ae63ba35884db43c38d924400b9dde8f7d
caio-fernadiz <arostuche@gmail.com>
2025-05-05 11:31:35 -0300

    criando endpoint de editar por id no bookcontroler

--- src/main/java/com/biblioteca/biblioteca_avaliar/book/BookController.java
+++ src/main/java/com/biblioteca/biblioteca_avaliar/book/BookController.java
@@ -4,13 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.biblioteca.biblioteca_avaliar.book.bookDTO.BookDTO;
 import com.biblioteca.biblioteca_avaliar.book.bookDTO.BookFormDTO;
+import com.biblioteca.biblioteca_avaliar.book.bookDTO.BookFormUpdateDTO;
 import com.biblioteca.biblioteca_avaliar.book.service.BookService;
 
 import jakarta.validation.Valid;
@@ -31,5 +34,11 @@ public class BookController {
     public ResponseEntity<BookDTO> saveBook(@Valid @RequestBody BookFormDTO bookFormDTO) {
         BookDTO bookDTO = bookService.saveBook(bookFormDTO);
         return new ResponseEntity<>(bookDTO, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<BookDTO> updateBookById(@PathVariable Long id, @RequestBody BookFormUpdateDTO updateForm){
+        BookDTO updateBookById = bookService.updateBookById(id, updateForm);
+        return ResponseEntity.ok(updateBookById);
     }
 }
